571225ad61dd10582b0c346d1ab132d673f1bcf2
EricsonRemocal05 <remocalericson05@gmail.com>
2020-12-06 14:09:19 +0800

    Fix some typo

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import { toggleCartHidden } from '../../redux/cart/cart.action';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
 
