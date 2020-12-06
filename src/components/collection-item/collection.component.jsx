f7911edba35f207351464c36f680ca8539b0c497
EricsonRemocal05 <remocalericson05@gmail.com>
2020-12-06 16:43:28 +0800

    updated shop data with new data, updated cart utils to group items in cart reducer and updated the shop data with unique id

--- src/components/collection-item/collection.component.jsx
+++ src/components/collection-item/collection.component.jsx
@@ -1,18 +1,31 @@
+import { connect } from 'react-redux';
+import CustomButton from '../custom-button/custom-button.component';
+import { addItemToCart } from '../../redux/cart/cart.actions';
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className='collection-item'>
-    <div
-      className='image'
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    <div className='collection-footer'>
-      <span className='name'>{name}</span>
-      <div className='price'>{price}</div>
+const CollectionItem = ({ item, addItemToCart }) => {
+  const { name, price, imageUrl } = item;
+  return (
+    <div className='collection-item'>
+      <div
+        className='image'
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className='collection-footer'>
+        <span className='name'>{name}</span>
+        <div className='price'>{price}</div>
+      </div>
+      <CustomButton onClick={() => addItemToCart(item)} inverted>
+        Add to cart
+      </CustomButton>
     </div>
-  </div>
-);
+  );
+};
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+  addItemToCart: (item) => dispatch(addItemToCart(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,8 +1,15 @@
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => (
   <button
-    className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
+    className={`${inverted ? 'inverted' : ''} custom-button ${
+      isGoogleSignIn ? 'google-sign-in' : ''
+    } custom-button`}
     {...otherProps}
   >
     {children}
