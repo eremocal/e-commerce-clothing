2e1bf3d156006110caf95213b3314e04bbc2da3d
EricsonRemocal05 <remocalericson05@gmail.com>
2020-12-12 15:17:42 +0800

    added cursor pointer

--- src/components/collection-preview/collection-preview.styles.jsx
+++ src/components/collection-preview/collection-preview.styles.jsx
@@ -9,6 +9,7 @@ export const CollectionPreviewContainer = styled.div`
 export const TitleContainer = styled.h1`
   font-size: 28px;
   margin-bottom: 25px;
+  cursor: pointer;
 
   &:hover {
     color: grey;
