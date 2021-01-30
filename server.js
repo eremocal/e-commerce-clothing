92e54a6164be9566e4e13b2af067eaa81591eb63
Ericson Remocal <remocalericson05@gmail.com>
2021-01-30 15:20:06 +0800

    added some vulnerability fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.REACT_APP_STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
